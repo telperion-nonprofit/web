b5ea65ccc1e4c01fcfebdbcee9f58b683977af5d
Anna Zezulka <anna.zezulka@proton.me>
2026-02-26 13:03:19 +0100

    Přidána preview verze navbaru

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  
-  // Tailwind 4 se dává sem, do Vite pluginů
   vite: {
     plugins: [tailwindcss()],
   },
